68dce6a5d2eed702e3ca338083b54524c12f6780
beto02001 <beto020017@gmail.com>
2025-10-03 00:42:41 -0600

    Initial Commit

---
+++ HabitForge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HabitForge
+//
+//  Created by Luis Humberto Martinez Echegaray on 03/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
